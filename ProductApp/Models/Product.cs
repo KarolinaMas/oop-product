48f30e03e620028103b3c1583c6edaf3361d2c9b
Karolina Maščinskaitė <kmkarole@yahoo.com>
2026-03-31 12:05:55 +0300

    add setter logic to all properties in class

--- ProductApp/Models/Product.cs
+++ ProductApp/Models/Product.cs
@@ -2,9 +2,38 @@ namespace ProductApp.Models
 {
     internal class Product
     {
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
+        private string name;
+        private double price;
+        private int quantity;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    name = value;
+            }
+        }
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 0)
+                    price = value;
+            }
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                if (value >= 0)
+                {
+                    quantity = value;
+                }
+            }
+        }
 
         public Product(string name, double price, int quantity)
         {
